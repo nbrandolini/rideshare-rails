f36ca4e97fb41b465e5573dabe6f9da7518b4412
Jackie <jonofre16@hotmail.com>
2018-04-05 13:42:36 -0700

    Added total_cost method to passenger controller

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,10 @@
 class Passenger < ApplicationRecord
-  has_many :trips dependent :destroy
+  has_many :trips, dependent: :destroy
+  validates :name, presence: true
+  validates :phone_num, presence: true
+
+  def total_cost
+    total = trips.inject(0) { |sum, trip| sum + trip.cost }
+    return total/100
+  end
 end
