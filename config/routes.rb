3666b19656432b34310f503e01d857d05d275b6c
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-05 15:48:20 -0700

    Nested route trips within passenger and added button to request trip

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'drivers#index'
   resources :drivers
 
-  resources :passengers
+  resources :passengers do
+    resources :trips, only: [:create]
+  end
 
   resources :trips, except: [:create]
 end
