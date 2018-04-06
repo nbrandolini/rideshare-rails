ae6679bb4caf639f772b7219cef4423345cd750e
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-06 01:01:02 -0700

    Change drivers html views

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -15,7 +15,7 @@ class DriversController < ApplicationController
   def create
     @driver = Driver.new(driver_params)
     if @driver.save
-      redirect_to driver_path
+      redirect_to drivers_path
     else
       render :new
     end
