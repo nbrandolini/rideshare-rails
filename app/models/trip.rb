ed0982699c360b4c497897ade7c100add4f09357
Jackie <jonofre16@hotmail.com>
2018-04-06 16:05:55 -0700

    Added validations to Edit Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
-  # validates :rating, numericality: { only_float: true }
+  validates :rating, numericality: { only_float: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :passenger_id, presence: true, numericality: { only_integer: true }
+  validates :driver_id, presence: true, numericality: { only_integer: true }
+
 
 end
